c12b966c3b163bf8c0052ec9aa12555a17e18f61
iceasdza <roiceasd@gmail.com>
2023-12-29 11:02:34 +0700

    add multiplier number api

--- api/index.js
+++ api/index.js
@@ -1,11 +1,16 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require("express");
+const app = express();
+const port = 3000;
 
-app.get('/api', (req, res) => {
-  res.send('Hello World!')
-})
+app.get("/api", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.get("/api/multiplier:number", (req, res) => {
+  const number = req.params.number;
+  res.send("your number is : ", number*2);
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
