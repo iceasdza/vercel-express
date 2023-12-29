3c4941781d90cc42492ec4aac08876a1e5a08444
iceasdza <roiceasd@gmail.com>
2023-12-29 11:04:27 +0700

    fix path

--- api/index.js
+++ api/index.js
@@ -6,7 +6,7 @@ app.get("/api", (req, res) => {
   res.send("Hello World!");
 });
 
-app.get("/api/multiplier:number", (req, res) => {
+app.get("/api/multiplier/:number", (req, res) => {
   const number = req.params.number;
   res.send("your number is : ", number*2);
 });
